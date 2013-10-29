2ce0d8737d316836941670d36d0d094c80c25687
Craig Condon <craig@spiceapps.com>
2013-10-28 18:01:49 -0700

    added bullets

--- public/packages/http.site/views/main/pages/index.pc.js
+++ public/packages/http.site/views/main/pages/index.pc.js
@@ -1,10 +1,13 @@
 module.exports = function(fragment, block, element, text, parse, modifiers) {
-    return fragment([ block({
+    return fragment([ element("div", {
+        "class": [ "container" ],
+        role: [ "main" ]
+    }, [ text(" "), block({
         html: {
             fn: function() {
                 return this.ref("sections.pages").value();
             },
             refs: [ "sections.pages" ]
         }
-    }), text(" ") ]);
+    }), text(" ") ]), text(" ") ]);
 };
--- src/packages/http.site/views/main/pages/home/index.coffee
+++ src/packages/http.site/views/main/pages/home/index.coffee
@@ -1,7 +1,7 @@
 mojo = require "mojojs"
 
 class HomeView extends mojo.View
-  
+  downloadLink: "https://github.com/classdojo/mojo-starter/archive/master.zip"
   paper: require("./index.pc")
 
 module.exports = HomeView 
