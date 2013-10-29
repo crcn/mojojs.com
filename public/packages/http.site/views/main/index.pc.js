c71d9d38b9da1e74134046785b07afaa95f3ebfd
Craig Condon <craig@spiceapps.com>
2013-10-28 18:13:35 -0700

    added ribbon

--- public/packages/http.site/views/main/index.pc.js
+++ public/packages/http.site/views/main/index.pc.js
@@ -26,7 +26,13 @@ module.exports = function(fragment, block, element, text, parse, modifiers) {
         href: [ "https://github.com/classdojo/mojo.js" ]
     }, [ text("Documentation") ]) ]), text(" "), element("li", {}, [ element("a", {
         href: [ "/plugins" ]
-    }, [ text("Plugins") ]) ]), text(" ") ]), text(" ") ]), text(" "), block({
+    }, [ text("Plugins") ]) ]), text(" ") ]), text(" "), element("a", {
+        href: [ "https://github.com/classdojo/mojo.js" ]
+    }, [ element("img", {
+        style: [ "position: absolute; top: 0; right: 0; border: 0;" ],
+        src: [ "https://s3.amazonaws.com/github/ribbons/forkme_right_gray_6d6d6d.png" ],
+        alt: [ "Fork me on GitHub" ]
+    }) ]), text(" ") ]), text(" "), block({
         html: {
             fn: function() {
                 return this.ref("sections.pages").value();
