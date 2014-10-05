8b3748f2d50f1817c0740086cb3f9b00300d07c9
Craig Condon <craig@spiceapps.com>
2014-10-05 00:35:18 -0700

    update docs

--- content/pages/home/_snippets/simple/index.js
+++ content/pages/home/_snippets/simple/index.js
@@ -1,8 +1,11 @@
+// commonjs & AMD are supported out of the box
 var HelloView = mojo.views.Base.extend({
   paper: require("./template.pc")
 });
 
 module.exports = function (element) {
   var view = new HelloView();
+
+  // render() returns a document fragment
   element.appendChild(view.render());
 }
