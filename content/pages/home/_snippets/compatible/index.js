10d4a41fc8a14c0bc50910681885b1f2c804e1a7
Craig Condon <craig@spiceapps.com>
2014-10-06 23:48:02 -0700

    add markdown example

--- content/pages/home/_snippets/compatible/index.js
+++ content/pages/home/_snippets/compatible/index.js
@@ -1,1 +1,14 @@
-// TODO
+var marked = require("marked");
+
+mojo.application.paperclip.modifier("markdown", marked);
+
+var MarkedView = mojo.views.Base.extend({
+    paper: require("./template.pc")
+});
+
+module.exports = function (element) {
+    var view = new MarkedView({
+        content: "Hello *world*, I'm **Markdown**!"
+    });
+    element.appendChild(view.render());
+}
